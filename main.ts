c29d40c78670be0e3f94387654495b0484107939
Hafiza4Asma <asmanadeem8543@gmail.com>
2024-06-01 20:05:38 -0700

    My 4th project
    todo_list

---
+++ main.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer"
+let todos : string [] = [];
+let condition = true;
+while(condition){
+let addTask = await inquirer .prompt(
+    [
+        {
+            name : "todo",
+            type : "input",
+            message : "What do you want to add in your todo?"
+        },
+        {
+            name : "addMore",
+            type : "confirm",
+            message : "Do you want to add more todo?",
+            default : "false"
+        }
+    ]
+);
+todos.push(addTask.todo);
+condition = addTask.addMore
+console.log(todos);
+};
